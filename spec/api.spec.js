e2dd76a939d6e709c3a01b0d29a115ed45e0a879
Taiwo Oyindamola <taiwooyindamola732@gmail.com>
2018-08-16 12:53:59 +0100

    Add test framework (jasmine)

---
+++ spec/api.spec.js
@@ -0,0 +1,123 @@
+const Request = require('request');
+
+describe('port', () => {
+  let port;
+  beforeAll(() => {
+    port = require('../api');
+  });
+
+  describe('GET /api/v1/questions', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.get('http://localhost:3000/api/v1/questions', (error, res, body) => {
+        data.status = res.statusCode;
+        data.body = body;
+        done();
+      });
+    });
+
+    it('status 200', () => {
+      expect(data.status).toBe(200);
+    });
+  });
+  describe('GET /api/v1/questions/:id', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.get('http://localhost:3000/api/v1/questions/:id', (error, res, body) => {
+        data.status = res.statusCode;
+        data.body = body;
+        done();
+      });
+    });
+    it('status 200', () => {
+      expect(data.status).toBe(200);
+    });
+  });
+
+  describe('GET /api/v1/questions/:id/answers', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.get('http://localhost:3000/api/v1/questions/:id/answers', (error, res, body) => {
+        data.status = res.statusCode;
+        data.body = body;
+        done();
+      });
+    });
+    it('status 200', () => {
+      expect(data.status).toBe(200);
+    });
+  });
+
+  describe('POST /api/v1/questions', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.post('http://localhost:3000/api/v1/questions', (error, res, body) => {
+        data.status = res.statusCode;
+        data.body = body;
+        done();
+      });
+    });
+    it('status 200', () => {
+      expect(data.status).toBe(200);
+    });
+  });
+
+  describe('POST /api/v1/questions/:id/answers', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.post('http://localhost:3000/api/v1/questions/:id/answers', (error, res, body) => {
+        data.status = res.statusCode;
+        data.body = body;
+        done();
+      });
+    });
+    it('status 200', () => {
+      expect(data.status).toBe(200);
+    });
+  });
+
+  describe('PUT /api/v1/questions/:id', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.put('http://localhost:3000/api/v1/questions/:id', (error, res, body) => {
+        data.status = res.statusCode;
+        data.body = body;
+        done();
+      });
+    });
+    it('status 200', () => {
+      expect(data.status).toBe(200);
+    });
+  });
+
+  describe('DELETE /api/v1/questions/:id', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.delete('http://localhost:3000/api/v1/questions/:id', (error, res, body) => {
+        data.status = res.statusCode;
+        data.body = body;
+        done();
+      });
+    });
+    it('status 200', () => {
+      expect(data.status).toBe(200);
+    });
+  });
+
+  describe('DELETE /api/v1/questions/:id/answers', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.delete('http://localhost:3000/api/v1/questions/:id/answers', (error, res, body) => {
+        data.status = res.statusCode;
+        data.body = body;
+        done();
+      });
+    });
+    it('status 200', () => {
+      expect(data.status).toBe(200);
+    });
+    it('content', () => {
+      expect(data.body).toBe('question with the ID cannot be found');
+    });
+  });
+});
